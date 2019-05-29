93028f72b5bdf2e76a6c0001ba5307b5e6b6bb1f
KonradK0 <konradkorus0@gmail.com>
2019-05-30 00:29:19 +0200

    Add grouping by 'comment' instead of 'rev_id', add logger

--- preprocessing/toxicity.py
+++ preprocessing/toxicity.py
@@ -3,9 +3,8 @@ import os.path
 
 import pandas as pd
 
-logging.basicConfig(level=logging.INFO, format='')
-
 DATA_DIR = os.path.join('..', 'data', 'toxicity')
+DATA_FILE = 'toxicity.csv'
 
 
 def join_datasets(*, comments_fname: str, annotations_fname: str) -> pd.DataFrame:
@@ -17,13 +16,13 @@ def join_datasets(*, comments_fname: str, annotations_fname: str) -> pd.DataFram
     comments = pd.read_csv(comments_path, sep='\t', usecols=['rev_id', 'comment'], dtype=dtypes)
     annotations = pd.read_csv(annotations_path, sep='\t', usecols=['rev_id', 'toxicity'], dtype=dtypes)
 
-    joined = pd.merge(comments, annotations, on='rev_id').groupby('rev_id')['toxicity'].agg(
+    joined = pd.merge(comments, annotations, on='rev_id').groupby(by='comment')['toxicity'].agg(
         pd.Series.mode).to_frame()
     joined = joined.loc[joined['toxicity'].astype(str).isin(['0', '1'])]
     return joined.astype(int)
 
 
-def get_toxicity_df(fpath):
+def get_toxicity_df(fpath: str) -> pd.DataFrame:
     if not os.path.exists(fpath):
         joined = join_datasets(comments_fname='toxicity_annotated_comments.tsv',
                                annotations_fname='toxicity_annotations.tsv')
@@ -34,6 +33,6 @@ def get_toxicity_df(fpath):
 
 
 if __name__ == '__main__':
-    fname = 'toxicity.csv'
-    joined = get_toxicity_df(fpath=os.path.join(DATA_DIR, fname))
-    logging.info(joined['toxicity'].value_counts())
+    logger = logging.getLogger(__name__)
+    joined = get_toxicity_df(fpath=os.path.join(DATA_DIR, DATA_FILE))
+    logger.info(joined['toxicity'].value_counts())
